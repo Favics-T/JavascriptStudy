ccbf1d0dbebb8edf8dc0f5c05c11b0b4877cc5d1
unknown <taiwokolapo533@gmail.com>
2025-04-18 03:42:15 +0100

    Fetched data from json placeholde

--- MiniAsyncProject/index.js
+++ MiniAsyncProject/index.js
@@ -4,11 +4,18 @@ async function fetchUser(){
         const data = await res.json();
 
         const usersList = document.getElementById('usersList');
-        
+      
+       data.map((user)=>{
+        let li = document.createElement('li');
+        li.textContent = user.name;
+        usersList.appendChild(li);
+       })
+       
+
 
     }
     catch(error){
-
+            console.error("error fetching data")
     }
 }
 fetchUser();
